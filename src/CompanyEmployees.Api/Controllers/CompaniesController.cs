1112301360c3d2a1ae1395fa1444f22e46f0f660
Abdulrahman <abdulrahmanhallak04@gmail.com>
2023-10-11 19:50:59 +0300

    Add the company controller with a get method

---
+++ src/CompanyEmployees.Api/Controllers/CompaniesController.cs
@@ -0,0 +1,23 @@
+using CompanyEmployees.Api.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CompanyEmployees.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CompaniesController : ControllerBase
+{
+    private readonly ICompanyService _service;
+
+    public CompaniesController(ICompanyService service)
+    {
+        _service = service;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetCompanies()
+    {
+        var companies = await _service.GetAllAsync(true);
+        return Ok(companies);
+    }
+}

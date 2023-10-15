5cf02d30b3cfe01f508bffae9a77463eac1f218e
Abdulrahman <abdulrahmanhallak04@gmail.com>
2023-10-16 00:00:14 +0300

    Add HttpPost endpoint for creating company entity

--- src/CompanyEmployees.Api/Controllers/CompaniesController.cs
+++ src/CompanyEmployees.Api/Controllers/CompaniesController.cs
@@ -1,4 +1,5 @@
 using CompanyEmployees.Api.Interfaces;
+using CompanyEmployees.Api.Models;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using OneOf.Types;
@@ -31,6 +32,17 @@ public class CompaniesController : ControllerBase
         (
             Ok,
             _ => NotFound($"There is no company with the provided {id}")
+        );
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateCompany(CompanyForCreateDto dto)
+    {
+        var result = await _service.CreateAsync(dto);
+        return result.Match<IActionResult>
+        (
+            company => CreatedAtAction(nameof(GetCompanies), new { id = company.Id }, company),
+            err => StatusCode(StatusCodes.Status500InternalServerError)
         );
     }
 }

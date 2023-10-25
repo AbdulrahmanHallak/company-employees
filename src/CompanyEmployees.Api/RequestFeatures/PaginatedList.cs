41c0d501bd7672c2a6c48943efac2a9d10b2fd70
Abdulrahman <abdulrahmanhallak04@gmail.com>
2023-10-25 11:39:13 +0300

    Removed inheritance to preserve type integrity and functionality
    Removed the inheritance due to the incompatibility of the type
    with List<T>'s methods such as adding and removing. These operations
    break the promise of the type, which is intended to be an immutable
    wrapper around a list with specific properties.
    Mutating the list or its properties would violate the fundamental
    design principles of this type.

--- src/CompanyEmployees.Api/RequestFeatures/PaginatedList.cs
+++ src/CompanyEmployees.Api/RequestFeatures/PaginatedList.cs
@@ -1,22 +1,24 @@
 using Microsoft.EntityFrameworkCore;
 
 namespace CompanyEmployees.Api.RequestFeatures;
-public class PaginatedList<T> : List<T>
+public class PaginatedList<T>
 {
     public int PageIndex { get; private set; }
     public int TotalPages { get; private set; }
+    public int PageSize { get; private set; }
+
+    public bool HasPreviousPage => PageIndex > 1;
+    public bool HasNextPage => PageIndex < TotalPages;
+
+    public IEnumerable<T> Data { get; private set; }
 
     public PaginatedList(List<T> items, int count, int pageIndex, int pageSize)
     {
+        PageSize = pageSize;
         PageIndex = pageIndex;
         TotalPages = (int)Math.Ceiling(count / (double)pageSize);
-
-        AddRange(items);
+        Data = items;
     }
-
-    public bool HasPreviousPage => PageIndex > 1;
-
-    public bool HasNextPage => PageIndex < TotalPages;
 
     public static async Task<PaginatedList<T>> CreateAsync(IQueryable<T> source, int pageIndex, int pageSize)
     {

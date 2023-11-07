a955cf509d1a081c933e13ac88cc3ac851c96854
Abdulrahman <abdulrahmanhallak04@gmail.com>
2023-11-07 15:48:28 +0300

    The package Microsoft.AspNetCore.Mvc.Versioning is deprecated

--- src/CompanyEmployees.Api/Extensions/Extensions.cs
+++ src/CompanyEmployees.Api/Extensions/Extensions.cs
@@ -1,8 +1,8 @@
+using Asp.Versioning;
 using CompanyEmployees.Api.Controllers;
 using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
-using Microsoft.AspNetCore.Mvc.Versioning;
 
 namespace CompanyEmployees.Api.Extensions;
 public static class Extensions
@@ -13,14 +13,19 @@ public static class Extensions
             modelState.AddModelError(error.PropertyName, error.ErrorMessage);
     }
     public static IServiceCollection ConfigureVersioning(this IServiceCollection services)
-        => services.AddApiVersioning(opts =>
+    {
+        services.AddApiVersioning(opts =>
         {
             opts.AssumeDefaultVersionWhenUnspecified = true;
             opts.ReportApiVersions = true;
             opts.DefaultApiVersion = new ApiVersion(1, 0);
             opts.ApiVersionReader = new HeaderApiVersionReader("api-version");
+        }).AddMvc(opts =>
+        {
             opts.Conventions.Controller<CompaniesController>().HasApiVersion(new ApiVersion(1, 0));
+            opts.Conventions.Controller<CompaniesV2Controller>().HasDeprecatedApiVersion(new ApiVersion(2, 0));
             opts.Conventions.Controller<EmployeesController>().HasApiVersion(new ApiVersion(1, 0));
-            opts.Conventions.Controller<CompaniesV2Controller>().HasApiVersion(new ApiVersion(2, 0));
         });
+        return services;
+    }
 }

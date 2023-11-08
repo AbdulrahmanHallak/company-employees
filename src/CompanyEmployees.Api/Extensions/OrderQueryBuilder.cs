b478bd26f40dfcc80eb6ff22d09fe6f25e897c08
Abdulrahman <abdulrahmanhallak04@gmail.com>
2023-11-08 11:50:09 +0300

    Add docs

--- src/CompanyEmployees.Api/Extensions/OrderQueryBuilder.cs
+++ src/CompanyEmployees.Api/Extensions/OrderQueryBuilder.cs
@@ -4,7 +4,23 @@ using System.Text;
 namespace CompanyEmployees.Api.Extensions;
 public static class OrderQueryBuilder
 {
-    public static string CreateOrderQuery<T>(string orderByQueryString)
+    /// <summary>
+    /// Creates an order query string based on the provided orderByQueryString for a specified type.
+    /// </summary>
+    /// <typeparam name="T">The type for which the order query string is being created.</typeparam>
+    /// <param name="orderByQueryString">The string containing order parameters.</param>
+    /// <returns>
+    /// A string representing the order query for the specified type based on the provided parameters.
+    /// </returns>
+    /// <remarks>
+    /// The returned string is only useful to use with System.Linq.Dynamic.Core library,
+    /// and it could be empty if the properties provided in the string
+    /// parameter do not exist in the specified type.
+    ///     Sample:
+    ///             orderByQueryString = "name desc,age"
+    ///             the method will return "name descending, age ascending"
+    /// </remarks>
+    public static string? CreateOrderQuery<T>(string orderByQueryString)
     {
         var orderParam = orderByQueryString.Trim().Split(',');
         var propertyInfos = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);

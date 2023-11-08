3d007546bae03f6fd03a5a386565afb2b01e65d6
Abdulrahman <abdulrahmanhallak04@gmail.com>
2023-11-08 20:12:02 +0300

    This method belongs in the configuration directory

--- src/CompanyEmployees.Api/Extensions/Extensions.cs
+++ src/CompanyEmployees.Api/Extensions/Extensions.cs
@@ -12,20 +12,4 @@ public static class Extensions
         foreach (var error in result.Errors)
             modelState.AddModelError(error.PropertyName, error.ErrorMessage);
     }
-    public static IServiceCollection ConfigureVersioning(this IServiceCollection services)
-    {
-        services.AddApiVersioning(opts =>
-        {
-            opts.AssumeDefaultVersionWhenUnspecified = true;
-            opts.ReportApiVersions = true;
-            opts.DefaultApiVersion = new ApiVersion(1, 0);
-            opts.ApiVersionReader = new HeaderApiVersionReader("api-version");
-        }).AddMvc(opts =>
-        {
-            opts.Conventions.Controller<CompaniesController>().HasApiVersion(new ApiVersion(1, 0));
-            opts.Conventions.Controller<CompaniesV2Controller>().HasDeprecatedApiVersion(new ApiVersion(2, 0));
-            opts.Conventions.Controller<EmployeesController>().HasApiVersion(new ApiVersion(1, 0));
-        });
-        return services;
-    }
 }
